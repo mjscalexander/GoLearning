aedef50c9e7bbcae0112bf05d48f7c170fc7e157
Martin Alexander <mjscalexander@gmail.com>
2023-04-03 05:38:08 +0000

    save

--- channels/main.go
+++ channels/main.go
@@ -1,11 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/eiannone/keyboard"
+)
+
+var keyPressChan chan rune
 
 func main(){
+	keyPressChan = make(chan rune)
 
+	go listenForKeyPress()
+
+	fmt.Println("Press any key, or q to quit")
+
+	_ = keyboard.Open()
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		keyPressChan <- char
+	}
 }
 
 func listenForKeyPress(){
+	for {
+		key := <-keyPressChan
+		fmt.Println("You entered", string(key))
+	}
 
 }
