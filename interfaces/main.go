c014c6b2e68e72101340d70f31f2d467f0fd5f7f
Martin Alexander <mjscalexander@gmail.com>
2023-04-08 02:24:48 +0000

    sve

---
+++ interfaces/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Dog struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+type Cat struct {
+	Name           string
+	Sound          string
+	NumberOfLegs   int
+	HasTail        bool
+}
+
+func main() {
+	
+}
+
