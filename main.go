46b0a8d5b68ccdd6a8640d40f3e9216d526b768b
Martin Alexander <mjscalexander@gmail.com>
2023-09-23 03:55:11 +0000

    clean up

--- main.go
+++ main.go
@@ -2,31 +2,9 @@ package main
 
 import (
 	"fmt"
-  "myapp/doctor"
-  "bufio"
-  "os"
-  "strings"
 )
 
 func main() {
-  reader := bufio.NewReader(os.Stdin)
-  
-  whatToSay := doctor.Intro()
-  
-  fmt.Println(whatToSay) 
-  for {
-    fmt.Print("--> ")
-    
-    userInput, _ := reader.ReadString('\n')
 
-    userInput = strings.Replace(userInput, "\r\n", "", -1)
-    userInput = strings.Replace(userInput, "\n", "", -1)
-    if userInput == "quit" {
-      break
-    } else {
-      fmt.Println(doctor.Response(userInput))
-    }
-
-  }
 
 }
