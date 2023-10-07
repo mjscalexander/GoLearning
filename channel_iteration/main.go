c1b83e50a88040f3ec7428167625a557815bfc22
Martin Alexander <mjscalexander@gmail.com>
2023-10-07 05:52:43 +0000

    channel iteration

--- channel_iteration/main.go
+++ channel_iteration/main.go
@@ -6,20 +6,21 @@ import (
 )
 
 func main() {
-	channel := make(chan string)
-	numRounds := 3
+	channel := make(chan string)	
+	go throwingNinjaStart(channel)
 	
-	go throwingNinjaStart(channel, numRounds)
-	
-	for i := 0 ; i < numRounds; i++ {
-		fmt.Println(<-channel)
+	for msg := range channel {
+		fmt.Println(msg)
 	}
-	
+
 }
 
-func throwingNinjaStart(channel chan string, numRounds int) {
-	for i := 0 ; i < numRounds; i++ {
+func throwingNinjaStart(channel chan string) {
+	numRounds := 3
+	for i:= 0 ; i < numRounds ; i++ {
 		score := rand.Intn(10)
 		channel <- fmt.Sprint("You scored: ", score)
 	}
+	close(channel)
+	// can't reopen a closed channel. you have to create a new one
 }
