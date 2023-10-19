b20d4010666433a8285a06e4cf0daf529cd75259
Martin Alexander <mjscalexander@gmail.com>
2023-10-19 23:42:25 +0000

    refreshing main

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"fmt"
-  "slices"
-)
-
 func main() {
-  lst := []string{"22", "1", "23","1", "25"}
-  for _, num := range lst {
-    flg, cache := uniqCache(num)
-    if flg {
-      fmt.Println("Id in list", cache)
-    } else {
-      fmt.Println("Id not in list, adding it...", cache)
-    }
-  }
-}
-
-func uniqCache(str string) (bool, []string) {
-  newlist := []string{"21"}
-  var flg bool
-  flg = false
-  func() {
-    if slices.Contains(newlist,str) != true {
-        newlist = append(newlist, str)
-      } else {
-        flg = true
-      }
-  }()
-  return flg, newlist
+  
 }
